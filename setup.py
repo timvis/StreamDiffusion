eb4e1c09d8fe897d08ac47a48c92bf48827ec551
timvis <tim@timvis.de>
2025-05-23 11:08:01 +0300

    Update setup.py
    changed requiered version of diffuser to be compatible with huggingface_hub

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 _deps = [
     "torch",
     "xformers",
-    "diffusers>=0.24.0,<=0.29.0",
+    "diffusers==0.29.0",
     "transformers",
     "accelerate",
     "fire",
